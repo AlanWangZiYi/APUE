d36d5a0e1d2083e1248cda39e9ba5d88a120fb3f
AlanWangZiYi <wangziyi128@126.com>
2018-03-22 19:28:32 +0800

    Unlink

---
+++ Chapter4/unlink.c
@@ -0,0 +1,16 @@
+#include "../include/apue.h"
+#include "../include/error.h"
+#include <fcntl.h>
+
+int main()
+{
+    if(open("tempfile", O_RDWR) < 0)
+        err_sys("open error");
+    if(unlink("tempfile") < 0)
+        err_sys("unlink error");
+    printf("file unlink\n");
+
+    sleep(10);
+    printf("down\n");
+    exit(0);
+}
